facbba2c52f922ac29e6ffb018411b81cedd0d37
github username <github email address>
2018-06-01 01:57:50 -0400

    Blackjack CLI

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,57 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
+  card_total
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  
+  sum = deal_card + deal_card
+
+  display_card_total(sum)
+
+  return sum
 end
 
-def hit?
-  # code hit? here
+def hit?(current_card_total)
+  
+  prompt_user
+  response = get_user_input
+
+  if response == "s"
+    return current_card_total
+  elsif response == "h"
+    num = deal_card
+    return num + current_card_total
+  else
+    invalid_command
+    prompt_user
+  end
+  
+  return current_card_total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +59,15 @@ end
 #####################################################
 
 def runner
-  # code runner here
+
+  welcome  
+  total = initial_round
+  
+  while total < 21
+    total = hit?(total)
+    total = display_card_total(total)
+  end
+  
+  end_game(total)
+
 end
-    
